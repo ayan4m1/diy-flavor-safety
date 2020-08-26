95a0ea4263578099bf322df6aaeb063e86bfde09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-26 12:48:06 +0000

    Deploy ayan4m1/diy-flavor-safety to github.com/ayan4m1/diy-flavor-safety.git:gh-pages

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ba4bb52a8bc311b579bf.js"
+    "url": "webpack-runtime-de2a60902de437a30df1.js"
   },
   {
     "url": "styles.3cf007c0c8f75c3ad234.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f22a1aec3ca7953c98550beb3fce0f5d"
+    "revision": "969f2a126cea4497ec3efe54b6a19004"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6d7462888018cb13a12a.js"
